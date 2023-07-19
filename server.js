bffa63508f604f09556ce825e9e6a5f17ef020b3
venu-shastri <venu.shastri@gmail.com>
2023-07-19 13:41:07 +0530

    server js updated

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ app.get('/',(req,res)=>{
     let today=new Date();
     let time=today.getHours()+":"+today.getMinutes()+":"+today.getSeconds();
 
-    let message=`Response From MicroService  , current time is ${time}`;
+    let message=`Response From MicroService V3 , current time is ${time}`;
     res.send(message);
 });
 app.listen(port,()=>{
-    console.log("Microservice One is running");
+    console.log("Microservice One version 3  is running");
 });
